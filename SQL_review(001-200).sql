1868a5e97babdf34007361192601eb60b7d06b91
jungmyung_kr <jungmyung.kr@gmail.com>
2022-04-09 22:35:41 +0900

    200제 143번 풀이

--- SQL_review(001-200).sql
+++ SQL_review(001-200).sql
@@ -1764,3 +1764,18 @@ begin
     end if;
 end;
 /
+
+
+--143. PL/SQL Basic Loop 이해하기
+-- PL/SQL의 Basic loop문으로 구구단 2단을 출력해보시오. 
+
+declare
+    v_count number(10) := 0;
+begin
+    loop
+        v_count := v_count +1;
+        dbms_output.put_line ('2 x ' || v_count || ' = ' || 2*v_count);
+        exit when v_count = 9;
+    end loop;
+end;
+/
