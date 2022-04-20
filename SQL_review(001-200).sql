f99a0c9ab40f49996cb763f044bbafbff96f587a
jungmyung_kr <jungmyung.kr@gmail.com>
2022-04-20 09:51:12 +0900

    200제 154번 풀이

--- SQL_review(001-200).sql
+++ SQL_review(001-200).sql
@@ -1949,3 +1949,26 @@ begin
     end if;
 end;
 /
+
+
+--154. 수학식 구현하기 3 (지수 함수)
+--밑수와 지수를 각각 물어보게 하고 계산된 지수 함수의 결과가 출력되게 하는 PL/SQL을 작정하시오.
+
+set serveroutput on 
+set verify off
+accept p_num1 prompt '밑수를 입력하세요.'
+accept p_num2 prompt '지수를 입력하세요.'
+
+declare
+    v_result number(10) := 1;
+    v_num2 number(10) := &p_num1;
+    v_count number(10) := 0;
+begin
+    loop
+        v_count := v_count + 1;
+        v_result := v_result * v_num2 ;
+        exit when v_count = &p_num2;
+    end loop;
+        dbms_output.put_line(v_result);
+end;
+/
