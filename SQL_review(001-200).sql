6edebbb246c05cbf9430a310f269a6e2c6c22a5a
jungmyung_kr <jungmyung.kr@gmail.com>
2022-04-22 23:00:55 +0900

    200제 155번 풀이

--- SQL_review(001-200).sql
+++ SQL_review(001-200).sql
@@ -1952,7 +1952,7 @@ end;
 
 
 --154. 수학식 구현하기 3 (지수 함수)
---밑수와 지수를 각각 물어보게 하고 계산된 지수 함수의 결과가 출력되게 하는 PL/SQL을 작정하시오.
+--밑수와 지수를 각각 물어보게 하고 계산된 지수 함수의 결과가 출력되게 하는 PL/SQL을 작성하시오.
 
 set serveroutput on 
 set verify off
@@ -1972,3 +1972,28 @@ begin
         dbms_output.put_line(v_result);
 end;
 /
+
+
+--155. 수학식 구현하기 4 (로그 함수)
+--밑수와 진수를 각각 물어보게 하고 계산된 로그 함수의 결과가 출력되게 하는 PL/SQL문을 작성하시오.
+
+set serveroutput on
+set verify off
+accept p_num1 prompt '밑수를 입력하세요.'
+accept p_num2 prompt '진수를 입력하세요.'
+
+declare
+        v_num1 number(10) := &p_num1;
+        v_num2 number(10) := &p_num2;
+        v_count number(10) := 0;
+        v_result number(10) := 1;
+begin
+    loop
+        v_count := v_count + 1;
+        v_result := v_result * v_num1 ;
+        exit when v_result = v_num2;
+    end loop;
+        dbms_output.put_line(v_count);
+end;
+/
+        
