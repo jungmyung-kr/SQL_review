e8edd48f564597a64925cca2c97e71e6d9c284fe
jungmyung_kr <jungmyung.kr@gmail.com>
2022-04-24 22:48:36 +0900

    200제 156번 풀이

--- SQL_review(001-200).sql
+++ SQL_review(001-200).sql
@@ -1997,3 +1997,24 @@ begin
 end;
 /
         
+
+--156. 수학식 구현하기 5 (순열)
+-- 수학식 순열을 PL/SQL로 구현하시오.
+
+set serveroutput on
+set verify off
+declare
+     v_name1   sample.fruit%type;
+     v_name2   sample.fruit%type; 
+begin
+  for  i  in  1 .. 3  loop
+     for  j  in  1 .. 3  loop 
+          select  fruit  into v_name1  from sample where num = i;
+          select  fruit  into v_name2  from sample where num = j;
+          if  i != j  then 
+             dbms_output.put_line(v_name1 ||', '|| v_name2);
+         end  if; 
+    end loop;
+ end loop;
+end;
+/
