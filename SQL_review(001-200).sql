24c353be0da3b880126676663a43fa7ba66d0a8f
jungmyung_kr <jungmyung.kr@gmail.com>
2022-04-26 02:44:05 +0900

    200제 158번 풀이

--- SQL_review(001-200).sql
+++ SQL_review(001-200).sql
@@ -2040,3 +2040,26 @@ begin
  end loop;
 end;
 /
+
+--158. 기초통계 구현하기 1 (평균값)
+--여러 개의 숫자를 입력받은 후 입력 받은 숫자들의 평균 값을 출력하는 PL/SQL문을 작성하시오.
+
+set serveroutput on
+set verify off
+accept p_arr prompt '숫자를 입력하세요.';
+ 
+declare
+    type arr_type is varray(5) of number(10);
+    v_num_arr   arr_type    := arr_type(&p_arr);
+    v_sum   number(10)  := 0;
+    v_cnt   number(10)  := 0;
+begin
+    for i in 1 .. v_num_arr.count loop
+        v_sum := v_sum + v_num_arr(i) ;
+        v_cnt := v_cnt +1;
+    end loop;
+    
+dbms_output.put_line(v_sum/v_cnt);
+
+end;
+/
