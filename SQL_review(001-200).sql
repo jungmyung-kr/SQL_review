3d8e75be2dd6d725ea2cf62f4a6cf4f2775613d6
jungmyung_kr <jungmyung.kr@gmail.com>
2022-05-06 23:06:42 +0900

    200제 164번 풀이

--- SQL_review(001-200).sql
+++ SQL_review(001-200).sql
@@ -2252,3 +2252,35 @@ begin
        dbms_output.put_line( '상관관계는: ' || v_corr ) ;
 end;
 /
+
+--164.  기초 통계 구현하기 7 (확률)
+-- 하나의 동전을 여러 번 던졌을 때 앞면과 뒷면이 나올 확률이 각각 50%임을 PL/SQL문로 확인해보시오.
+
+set serveroutput on
+set verify off
+
+declare
+    v_loop number(10) := 10000;
+    v_coin number(10) ;
+    v_0  number(10) := 0 ;
+    v_1  number(10) := 0 ; 
+
+begin
+    for i in 1..v_loop loop    
+
+        select  round(dbms_random.value(1,2)) into v_coin
+              from dual;
+
+        if  v_coin = 1 then 
+            v_0 := v_0 + 1;
+
+        else  
+            v_1 := v_1+1;
+     
+        end if;
+    end loop;
+
+    dbms_output.put_line('동전이 앞면이 나올 확률: '|| round((v_0/v_loop),2));
+    dbms_output.put_line('동전이 뒷면이 나올 확률: '|| round((v_1/v_loop),2));  
+end;
+/
