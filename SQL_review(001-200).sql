1d999c5cb3789962d4de1968d7a5e590d74dfefe
jungmyung_kr <jungmyung.kr@gmail.com>
2022-05-09 23:22:43 +0900

    200제 167번 풀이

--- SQL_review(001-200).sql
+++ SQL_review(001-200).sql
@@ -2359,3 +2359,47 @@ end;
 SELECT level-1 grade, mybin(level-1) 확률, lpad('■', mybin(level-1)*100, '■') "막대그래프"
   FROM dual
   CONNECT BY level < 12;
+
+
+--167.  기초 통계 구현하기 10 (정규분포)
+-- 초등학생 십만 명의 키 숫자 데이터를 랜덤으로 생성하여 
+-- 십만 명의 키 데이터가 정규분포를 이루는지 PL/SQL로 구현해보시오.
+
+set serveroutput on
+
+create or replace procedure probn
+ ( p_mu in number,
+   p_sig in number,
+   p_bin in number)
+is
+
+   type arr_type is varray(9) of number(30);
+
+  v_sim number(10) := 10000;
+  v_rv number(20,7);
+  v_mu number(10) := p_mu;
+  v_sig number(10) := p_sig;
+  v_nm arr_type := arr_type('',0,0,0,0,0,0,0,'');
+  v_cnt arr_type := arr_type(0,0,0,0,0,0,0,0);
+  v_rg arr_type := arr_type(-power(2,31),-3,-2,-1,0,1,2,3,power(2,32));
+
+begin
+   for i in v_nm.first+1..v_nm.last-1 loop
+       v_nm(i) := v_mu-3*p_bin+(i-2)*p_bin;
+   end loop;
+
+  for i in 1..v_sim loop
+      v_rv := dbms_random.normal*v_sig+v_mu;
+
+        for i in 2..v_rg.count loop
+             if v_rv >= v_mu+v_rg(i-1)*p_bin and v_rv < v_mu+v_rg(i)*p_bin then
+                    v_cnt(i-1) := v_cnt(i-1)+1;
+             end if;
+        end loop;
+  end loop;
+
+  for i in 1..v_cnt.count loop
+dbms_output.put_line(rpad(v_nm(i)||'~'||v_nm(i+1), 10, ' ')||lpad('■',trunc((v_cnt(i)/v_sim)*100),'■'));
+  end loop;
+end;
+/
