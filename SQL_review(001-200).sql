2af2748e95663d8bdeb142a326954dd2f1560977
jungmyung_kr <jungmyung.kr@gmail.com>
2022-05-11 18:22:21 +0900

    200제 169번 풀이

--- SQL_review(001-200).sql
+++ SQL_review(001-200).sql
@@ -2419,3 +2419,17 @@ begin
     end loop;
 end;
 /
+
+
+--169. 알고리즘 문제 풀기 2 (사각형 출력)
+
+set serveroutput on 
+accept p_a prompt '가로의 숫자를 입력하세요.'
+accept p_b prompt '세로의 숫자를 입력하세요.'
+
+begin 
+   for  i  in  1 .. &p_b loop
+      dbms_output.put_line( lpad('★',&p_a,'★') );
+   end loop;
+end;
+/
