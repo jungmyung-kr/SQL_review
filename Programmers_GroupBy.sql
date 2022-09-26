0237bc136edbddfb52d131212eee036d2a73c7ab
Jungmyung Kim <jungmyung.kr@gmail.com>
2022-09-26 17:31:55 +0900

    프로그래머스 문제풀이 GROUPBY문 3번

--- Programmers_GroupBy.sql
+++ Programmers_GroupBy.sql
@@ -7,6 +7,10 @@
 -- ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 
 -- 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
 
+-- ANIMAL_OUTS 테이블은 동물 보호소에서 입양 보낸 동물의 정보를 담은 테이블입니다. 
+-- ANIMAL_OUTS 테이블 구조는 다음과 같으며, 
+-- ANIMAL_ID, ANIMAL_TYPE, DATETIME, NAME, SEX_UPON_OUTCOME는 각각 동물의 아이디, 생물 종, 입양일, 이름, 성별 및 중성화 여부를 나타냅니다.
+
 -- 1. 고양이와 개는 몇 마리 있을까
 -- 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성해주세요. 
 -- 이때 고양이를 개보다 먼저 조회해주세요.
@@ -26,3 +30,13 @@ WHERE NAME IS NOT NULL
 GROUP BY NAME
 HAVING COUNT(NAME) >= 2
 ORDER BY NAME ASC;
+
+-- 3. 보호소에서는 몇 시에 입양이 가장 활발하게 일어나는지 알아보려 합니다. 
+-- 09:00부터 19:59까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요. 
+-- 이때 결과는 시간대 순으로 정렬해야 합니다.
+
+SELECT HOUR(DATETIME) AS HOUR, COUNT(DATETIME) AS COUNT 
+FROM ANIMAL_OUTS
+WHERE HOUR(DATETIME) BETWEEN 9 AND 19
+GROUP BY HOUR(DATETIME)
+ORDER BY HOUR(DATETIME) ASC;
